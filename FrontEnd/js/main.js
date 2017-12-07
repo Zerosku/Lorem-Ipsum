3f63fc1fb08a34e64317777633fd0623ab29245e
goebers <kack@cock.li>
2017-12-07 12:53:50 +0200

    look gooder

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -5,10 +5,16 @@ const hamMenu = document.querySelector('.fa-bars');
 const sidebar = document.querySelector('.sidebar');
 const file = document.querySelectorAll('.file');
 const folder = document.querySelectorAll('.folder');
+const lightbox = document.querySelector('.lightbox');
 
 // create new DOM elements
 const dropDiv = document.createElement('div'); // container div element
 dropDiv.className = 'dropdown';
+
+const downloadLink = document.createElement('a'); // download button
+const downloadText = document.createTextNode('Download');
+downloadLink.appendChild(downloadText);
+downloadLink.className = 'smallButton';
 
 const favLink = document.createElement('a'); // favorite button
 const favText = document.createTextNode('Favourite');
@@ -27,9 +33,9 @@ hamMenu.addEventListener('click', (evt) => {
   sidebar.classList.toggle('hidden');
 });
 
-
+// click listener for various things
 document.addEventListener('click', (evt) => {
-    let click = evt.target;
+    let click = evt.target; // target that is clicked
 
     // checks if you clicked on a info bubble
     // and creates a little dropdown menu
@@ -38,6 +44,8 @@ document.addEventListener('click', (evt) => {
 
       click.className = 'fa fa-2x fa-times-circle';
 
+      // here we construct the dropdown menu from components above
+      dropDiv.appendChild(downloadLink);
       dropDiv.appendChild(favLink);
       dropDiv.appendChild(deleteLink);
       click.appendChild(dropDiv);
@@ -45,5 +53,7 @@ document.addEventListener('click', (evt) => {
 
       click.className = 'fa fa-2x fa-info-circle';
       dropDiv.remove();
+    } else if (click.className.includes('upload')) {
+      lightbox.classList.toggle('hidden'); // lightbox toggle
     }
 });
